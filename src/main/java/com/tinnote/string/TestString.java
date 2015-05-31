0d4afc1590609ffc9a99b15e9440b3f36da4df5a
apachexiong <apachexiong@gmail.com>
2015-05-31 23:10:26 +0800

    String custom split

--- src/main/java/com/tinnote/string/TestString.java
+++ src/main/java/com/tinnote/string/TestString.java
@@ -20,6 +20,8 @@
  *
  * StringTokenizer 的性能在tokens数量少的情况下由于String.split, 当tokens的数量超过900时，性能率低于split
  *
+ * 手工实现的split性能高于内置String.split和StringTokenizer
+ *
  */
 package com.tinnote.string;
 
@@ -38,7 +40,7 @@ public class TestString {
 
     static {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 900; i++) {
+        for (int i = 0; i < 10000; i++) {
             sb.append("h" + i);
             sb.append(";");
         }
@@ -46,9 +48,10 @@ public class TestString {
     }
 
     @Test
-    public void testString(){
+    public void testString() {
         testStringSplit();
         testStringTokenizer();
+        testCustomSplit();
     }
 
     /**
@@ -58,10 +61,6 @@ public class TestString {
      * 10000 tokens  mills: 6268 seconds:6 minutes:0
      * 10000 tokens  mills: 85821 seconds:85 minutes:1
      * 100000 tokens   mills: 107460 seconds:107 minutes:1
-     *
-     *
-     *
-     *
      */
 
     @Test
@@ -80,23 +79,65 @@ public class TestString {
      * 100 tokens mills: 95 seconds:0 minutes:0
      * 1000 tokens  mills: 689 seconds:0 minutes:0
      * 10000 tokens  mills: 8888 seconds:8 minutes:0
-     *
-     *
-     *
      */
 
     @Test
-    public void testStringTokenizer(){
+    public void testStringTokenizer() {
 
         long before = now();
         StringTokenizer stringTokenizer = new StringTokenizer(testSplitString, ";");
-        for(int i=0; i< 10000; i++){
-            while(stringTokenizer.hasMoreTokens()){
+        for (int i = 0; i < 10000; i++) {
+            while (stringTokenizer.hasMoreTokens()) {
                 stringTokenizer.nextToken();
             }
             stringTokenizer = new StringTokenizer(testSplitString, ";");
         }
         println(interval(before));
+
+    }
+
+    @Test
+    public void testCustomSplit() {
+
+        long before = now();
+        for (int i = 0; i < 10000; i++) {
+            splitStr(testSplitString, ";");
+        }
+        println(interval(before));
+
+    }
+
+    /**
+     * 10 tokens mills: 23 seconds:0 minutes:0
+     * 100 tokens mills: 52 seconds:0 minutes:0
+     * 1000 tokens  mills: 307 seconds:0 minutes:0
+     * 10000 tokens  mills: 3375 seconds:3 minutes:0
+     *
+     *
+     * 通过index实现indexOf
+     * @param str
+     * @param token
+     * @return
+     */
+    public static String[] splitStr(String str, String token) {
+        int strLength = str.length();
+        int tokenLength = token.length();
+
+        String[] splitStrs = new String[strLength];
+        int size = 0;
+        String leftStrs = str;
+
+        int startIndex = 0;
+        int endIndex = 0;
+
+        while (startIndex < strLength && (endIndex = leftStrs.indexOf(token, startIndex)) > 0) {
+            splitStrs[size++] = leftStrs.substring(startIndex, endIndex);
+            startIndex = endIndex + tokenLength;
+        }
+
+        String[] result = new String[size];
+        System.arraycopy(splitStrs, 0, result, 0, size);
+        return splitStrs;
 
     }
 
